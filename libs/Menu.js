c1e1d2bae5d65f19d7bd70dd381bdf88de992ad3
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-06-19 18:01:32 -0500

    clean up code

--- libs/Menu.js
+++ libs/Menu.js
@@ -18,20 +18,17 @@ class Menu {
     setDataService(option) {
         const idx = this.MENU.indexOf(option);
         if (idx == 2)
-            this.dataController.modifyData = true;
+            this.dataService.modifyData = true;
         if(idx == 1 || idx == 6 || idx == 4)
-            this.dataController.addNewData = true;
-        if (idx < 3) {
-            this.dataController.id = 2;
-        }
-        else if (idx < 5) {
-            this.dataController.id = 1;
-        }
+            this.dataService.addNewData = true;
+        if (idx < 3) 
+            this.dataService.id = 2;
+        else if (idx < 5) 
+            this.dataService.id = 1;
         else if (idx < 7)
-            
-            this.dataController.id = 0;
+            this.dataService.id = 0;
         else
-            this.dataController.id = -1;
+            this.dataService.id = -1;
     }
 
     getDataService() {
