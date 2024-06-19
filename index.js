4d97e5f61cc5b58928b2f3feaa445be48183e8d6
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-06-19 18:46:42 -0500

    clean code

--- index.js
+++ index.js
@@ -7,64 +7,27 @@ const Menu = require("./libs/Menu");
 
 const crud = new Crud("postgres", "P@$$word170195");
 const menu = new Menu();
+const menuList = menu.getMenuList();
+const sections = menu.getSectionList();
 
-/** DATA FIELD **/
-const TITILES = [
-    `,-----------------------------------------------.\n|                ${colors.yellow("Employee Manager")}               |\n\`-----------------------------------------------'`,
-    `\n,-----------------------------------------------.\n|                ${colors.yellow("Department Data Base")}               |\n\`-----------------------------------------------'`
-]
-// change user name and password by prompting user.
-// const pool = new Pool({
-//     user: "postgres",
-//     password: "P@$$word170195",
-//     host: "localhost",
-//     database: "employee_db"
-// });
-// pool.connect();
-
-const getOption = async () => {
-    console.log(TITILES[0]);
+const prompt = async () => {
+    console.log(sections[0]);
     const res = await inquirer.prompt([
         {
             type: "list",
             name: "option",
-            choices: menu.getMenu(),
+            choices: menuList,
             message: "What would you like to do?"
         }
     ]);
     menu.setDataService(res.option);
 }
 
-//     const id = getOptionHelper(res.option);
-//     return id;
-// }
-
-// const getOptionHelper = (option) => {
-//     for(let i = 0; i < MENU.length; i++){
-//         if(option == MENU[i]) return i;
-//     }
-
-//     return -1;
-// }
-
-// rows: [ {id: #, name: " "}, { ... }, ..., { ... }];
-// const viewData = async (tableName) => {
-//     const data = await pool.query(`select * from ${tableName}`, (error, {rows}) => {
-//         console.log(`first element: ${rows[0].name}`);
-//         return rows;
-//     });
-//     return data;
-// }
-
-// const render = (data, tableName) => {
-
-// }
-
 const driver = async () => {
     try {
         let isRunning = true;
         while (isRunning) {
-            await getOption();
+            await prompt();
             const dataService = menu.getDataService();
             if (dataService.modifyData) {
                 // invoke modify data function
@@ -73,19 +36,19 @@ const driver = async () => {
             } else if (dataService.id != -1) {
                 await crud.setOutputData(dataService.id);
                 const outputData = crud.getOutputData();
-                displayData(outputData);
+                displayData(sections[dataService.id + 1], outputData);
             }
-            // const data = await getData("departments")
-            // console.log(data);
             else isRunning = false;
         }
     } catch (error) {
         console.log(`${colors.red("ERROR occurs")}\n`, error);
     }
     console.log(`${colors.green("\nThank you for using our application")}`);
+    process.exit(0);
 }
 
-const displayData = (outputData) => {
+const displayData = (section, outputData) => {
+    console.log(section);
     console.log(outputData.title);
     console.log(outputData.content);
 }
