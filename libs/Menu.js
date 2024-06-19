b835dd367d5a4348390ad6e3f1c26e7a3bd1f51c
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-06-19 18:46:11 -0500

    modify menu script to have proper data fields.

--- libs/Menu.js
+++ libs/Menu.js
@@ -1,9 +1,15 @@
+const colors = require("colors");
+
 class Menu {
     constructor() {
         this.MENU = ["View All Employees", "Add Employee", "Update Employee Role",
             "View All Roles", "Add Role",
             "View All Departments", "Add Deperatment",
             "Exit"];
+        this.SECTIONS = [
+            `,-----------------------------------------------.\n|                ${colors.yellow("Employee Manager")}               |\n\`-----------------------------------------------'`,
+            `\n,-----------------------------------------------.\n|              ${colors.yellow("Department Data Base")}             |\n\`-----------------------------------------------'`
+        ]
         this.dataService = {
             id: 0,
             addNewData: false,
@@ -11,19 +17,23 @@ class Menu {
         }
     }
 
-    getMenu() {
+    getMenuList() {
         return this.MENU;
+    }
+
+    getSectionList() {
+        return this.SECTIONS;
     }
 
     setDataService(option) {
         const idx = this.MENU.indexOf(option);
         if (idx == 2)
             this.dataService.modifyData = true;
-        if(idx == 1 || idx == 6 || idx == 4)
+        if (idx == 1 || idx == 6 || idx == 4)
             this.dataService.addNewData = true;
-        if (idx < 3) 
+        if (idx < 3)
             this.dataService.id = 2;
-        else if (idx < 5) 
+        else if (idx < 5)
             this.dataService.id = 1;
         else if (idx < 7)
             this.dataService.id = 0;
