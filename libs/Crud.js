bb0f0c8a5ac09c298332be07bdeb17d91eb4722b
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-06-21 02:55:36 -0500

    fix QUERY_PARAMETERS array

--- libs/Crud.js
+++ libs/Crud.js
@@ -7,7 +7,7 @@ class Crud {
             "SELECT e.first_name, e.last_name, r.title, r.salary, d.name AS department, COALESCE(m.first_name || ' ' || m.last_name, 'NULL') AS manager FROM employees e JOIN roles r ON e.role_id = r.id JOIN departments d ON r.department = d.id LEFT JOIN employees m ON e.manager_id = m.id"];
         this.QUERY_PARAMETERS = [{ columnNames: "(name)", parameter: "($1)" },
         { columnNames: "(title, salary, department)", parameter: "($1, $2, $3)" },
-        { columnNames: "(first_name, last_name, role_id, manager_id)", parameter: "($1, $2, $3, $4" }];
+        { columnNames: "(first_name, last_name, role_id, manager_id)", parameter: "($1, $2, $3, $4)" }];
         this.pool = new Pool({
             user: `${userName}`,
             password: `${password}`,
